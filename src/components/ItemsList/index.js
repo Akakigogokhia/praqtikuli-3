022af22580c713e0a84a32179ef8a43684f91347
unknown <akaki.gogokhia.1@btu.edu.ge>
2023-12-10 12:53:01 +0400

    changed add to cart logic

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -3,12 +3,22 @@ import Item from '../Item';
 import './itemlist.css';
 
 const ItemList = ({ data }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(localStorage.getItem('cart')) ?? []
+  );
 
   const addToCart = (id, count) => {
-    const item = data.find((item) => item.id === id);
-    item.count = count;
-    setCartItems([...cartItems, item]);
+    const itemIndex = cartItems.findIndex((item) => item.id === id);
+    if (itemIndex >= 0) {
+      const items = cartItems.map((item, index) =>
+        index === itemIndex ? { ...item, count: item.count + count } : item
+      );
+      setCartItems(items);
+    } else {
+      const item = data.find((item) => item.id === id);
+      item.count = count;
+      setCartItems([...cartItems, item]);
+    }
   };
 
   useEffect(() => {
